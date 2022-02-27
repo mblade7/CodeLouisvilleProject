2201eec8b41e9515b494f4b09178ea0db06873b5
Michael Blaydes <mcblaydesconsulting@gmail.com>
2022-02-27 14:46:58 -0500

    Add project files.

---
+++ CodeLouisville Project/Compensation Tracker.cs
@@ -0,0 +1,26 @@
+﻿using CodeLouisvilleLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compensation_Tracker
+{
+    public class CompensationTracker : CodeLouisvilleAppBase
+    {
+        public CompensationTracker() : base("Compensation Tracker")
+        {
+        }
+
+        protected override bool Main()
+        {
+            Menu menu = new Menu();
+            menu.AddMenuItem("1","Check Compensation");
+            menu.AddMenuItem("2", "Quit");
+
+            string userSelection = Prompt4MenuItem("Select an option", menu);
+            return Prompt4YesNo("Would you like to do anything else (Y/N)?");
+        }
+    }
+}
---
+++ CodeLouisville Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodeLouisvilleClassLibrary;
+using CodeLouisvilleLibrary;
+using Compensation_Tracker;
+namespace Project
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            CompensationTracker compensationTracker = new CompensationTracker();
+            compensationTracker.Run();
+        }
+    }
+}
