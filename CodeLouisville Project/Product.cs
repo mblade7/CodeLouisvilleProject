66aa7deff92fdc75239ec1d5a9d7c9367a0b5c52
Michael Blaydes <mcblaydesconsulting@gmail.com>
2022-03-06 15:28:43 -0500

    Added new tools/Created Product Class

---
+++ CodeLouisville Project/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeLouisvilleLibrary.Serialization.Interfaces;
+
+namespace Product
+{
+    public class Product : IEntityWithID
+    {
+       public int ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+       public string Name { get; set;}
+       public double RegularPrice { get; set;}
+       public double Cost { get; set;}
+    }
+}
---
+++ Tools for Use/Serialization/EntitySerializationService.cs
@@ -0,0 +1,104 @@
+﻿using CodeLouisvilleLibrary.Serialization.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CodeLouisvilleLibrary.Serialization
+{
+    public class EntitySerializationService<T> where T : IEntityWithID
+    {
+        private string FileName;
+
+        public EntitySerializationService(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public async Task<T> SaveAsync(T item)
+        {
+            List<T> items = new List<T>();
+
+            if (!Directory.Exists(Path.GetDirectoryName(FileName)))
+                Directory.CreateDirectory(Path.GetDirectoryName(FileName));
+
+            using (Stream stream = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                if (File.Exists(FileName) && stream.Length > 0)
+                {
+                    items = await JsonSerializer.DeserializeAsync<List<T>>(stream) ?? new List<T>();
+                }
+
+                if (item.ID <= 0)
+                    item.ID = items.Count > 0 ? items.Max(i => i.ID) + 1 : 1;
+                else
+                    items.RemoveAll(i => i.ID == item.ID);
+
+                items.Add(item);
+
+                stream.SetLength(0); // rewrite instead of appending to the stream
+
+                var options = new JsonSerializerOptions { WriteIndented = true };
+
+                await JsonSerializer.SerializeAsync(stream, items, options);
+            }
+
+            return item;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            if (File.Exists(FileName))
+            {
+                using FileStream stream = File.OpenRead(FileName);
+
+                return await JsonSerializer.DeserializeAsync<IEnumerable<T>>(stream);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task DeleteAsync(int ID)
+        {
+            await DeleteAsync(await GetByID(ID));
+        }
+
+        public async Task DeleteAsync(T item)
+        {
+            if (item != null)
+            {
+                List<T> items = new List<T>();
+
+                if (!Directory.Exists(Path.GetDirectoryName(FileName)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(FileName));
+
+                using (Stream stream = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    if (File.Exists(FileName) && stream.Length > 0)
+                    {
+                        items = await JsonSerializer.DeserializeAsync<List<T>>(stream) ?? new List<T>();
+                    }
+
+                    items.RemoveAll(i => i.ID == item.ID);
+
+                    stream.SetLength(0); // rewrite instead of appending to the stream
+
+                    var options = new JsonSerializerOptions { WriteIndented = true };
+
+                    await JsonSerializer.SerializeAsync(stream, items, options);
+                }
+            }
+        }
+
+        public async Task<T> GetByID(int ID)
+        {
+            var list = await GetAllAsync();
+            return list.FirstOrDefault(e => e.ID == ID);
+        }
+
+
+    }
+}
